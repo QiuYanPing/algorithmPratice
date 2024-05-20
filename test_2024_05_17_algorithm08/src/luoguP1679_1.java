2298df2a26c2d9a382b182020d7e48c1a5559242
Qiu Yan ping <2740123221@qq.com>
2024-05-20 23:21:42 +0800

    完全背包超时问题的解决，剪枝

--- test_2024_05_17_algorithm08/src/luoguP1679_1.java
+++ test_2024_05_17_algorithm08/src/luoguP1679_1.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 /**
  * TLE超时
+ * 改进：剪枝
  */
 public class luoguP1679_1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int m = scanner.nextInt();
+		int x = (int)Math.sqrt((int)Math.sqrt(m));
 		int[] dp = new int[m+1];
 		//初始化
-		for (int i = 1; i < m+1; i++) {
+		for (int i = 1; i < dp.length; i++) {
 			dp[i] = Integer.MAX_VALUE;
 		}
 //		for (int i = 0; i < m+1; i++) {
 //			dp[i][0] = 0;
 //		}
 		
-		for (int i = 1; i < dp.length; i++) {
+		for (int i = 1; i <= x+1; i++) {
 			for (int j = 1; j < dp.length; j++) {
 				if (Math.pow(i, 4)>j)
 					continue;
